e8b08440e24664cf56a8ebdfc4decbdeaa7ab19c
tlovrekovic <tomislavl210@hotmail.com>
2022-11-08 18:18:05 +0100

    Cijeli prvi

---
+++ RUAPLV/RUAPLV/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RUAPLV.Models;
+using RUAPLV.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RUAPLV.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+    }
+}
